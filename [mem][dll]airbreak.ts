33ca4101395e29b3ed072ff9b45350c79cb1ad63
msx98 <me@glub.xyz>
2023-02-20 02:51:44 +0200

    Fix z axis changing, add option to change speed using spacebar

--- [mem][dll]airbreak.ts
+++ [mem][dll]airbreak.ts
@@ -6,25 +6,42 @@ const VK_S = 0x53;
 const VK_D = 0x44;
 const VK_UP = 0xA0;
 const VK_DN = 0xA2;
+const VK_SPACE = 0x20;
 var toggle = 0;
 var library;
 
+
+var SPEED_ARRAY = [0.5,5,50];
+var SPEED_IDX = 0;
+var SPEED = SPEED_ARRAY[SPEED_IDX];
+function next_speed() {
+    SPEED_IDX = (SPEED_IDX+1)%(SPEED_ARRAY.length);
+    SPEED = SPEED_ARRAY[SPEED_IDX];
+    Text.PrintStringNow(`Airbreak SPEED: ${SPEED}`, 500);
+}
+
+
 var TOGGLE_INTERVAL_MIN = 100;
-var TOGGLE_LAST_TIMESTAMP = 0;
 
-var PRESSED_FORWARD = 0;
-var PRESSED_BACKWARD = 0;
-var PRESSED_LEFT = 0;
-var PRESSED_RIGHT = 0;
-
-var SPEED = 10;
-
-var CYCLE_LENGTH = 1; //ms
 var CYCLES_SINCE_LAST_TOGGLE = 0;
+var CYCLES_SINCE_LAST_SPEED_CHANGE = 0;
 
 
 const PLAYER = new Player(0);
 var PLAYER_CHAR = PLAYER.getChar();
+
+function freeze(value) {
+    if (value == true) {
+        PLAYER_CHAR.freezePositionAndDontLoadCollision(true);
+        PLAYER_CHAR.freezePosition(true);
+        PLAYER_CHAR.setCollision(false);
+    } else {
+        PLAYER_CHAR.freezePositionAndDontLoadCollision(false);
+        PLAYER_CHAR.freezePosition(false);
+        PLAYER_CHAR.setCollision(true);
+    }
+
+}
 
 PLAYER_CHAR.setDrownsInWater(false);
 
@@ -39,18 +56,18 @@ while (true) {
         CYCLES_SINCE_LAST_TOGGLE = 0;
         toggle = toggle ^ 1;
         if (toggle == 1) {
-            //PLAYER.getChar().freezePosition(true);
-            PLAYER_CHAR.freezePositionAndDontLoadCollision(true);
-            PLAYER_CHAR.setCollision(false);
+            freeze(true);
             Text.PrintStringNow("Airbreak ~g~ENABLED", 1000);
         } else {
-            //PLAYER.getChar().freezePosition(false);
-            PLAYER_CHAR.freezePositionAndDontLoadCollision(false);
-            PLAYER_CHAR.setCollision(true);
+            freeze(false);
             Text.PrintStringNow("Airbreak ~r~DISABLED", 1000);
         }
     }
     if (toggle == 1) {
+        if (Pad.IsKeyPressed(VK_SPACE) && CYCLES_SINCE_LAST_SPEED_CHANGE >= 10) {
+            CYCLES_SINCE_LAST_SPEED_CHANGE = 0;
+            next_speed();
+        }
         pos = PLAYER_CHAR.getCoordinates();
         cam_pos = Camera.GetActiveCoordinates();
         cam_point_pos = Camera.GetActivePointAt();
@@ -67,6 +84,7 @@ while (true) {
         // if we are looking north, then y=1
         // east, y = 0
         // etc
+        //PLAYER_CHAR.setRotation(cam_vector.x, cam_vector.y, cam_vector.z);
         delta_x = 0; delta_y = 0; delta_z = 0;
         if (Pad.IsKeyPressed(VK_UP))
             delta_z += SPEED;
@@ -88,13 +106,16 @@ while (true) {
             delta_x -= SPEED*cam_vector.y;
             delta_y += SPEED*cam_vector.x;
         }
-        PLAYER_CHAR.freezePositionAndDontLoadCollision(false);
-        PLAYER_CHAR.setCollision(true);
-        PLAYER_CHAR.setRotation(1,0,0);
-        PLAYER_CHAR.setVelocity(delta_x,delta_y,delta_z);
-        Text.PrintStringNow(`Going towards ${cam_vector.x}, ${cam_vector.y}, ${cam_vector.z}`,10);
+        if ((delta_x != 0) || (delta_y != 0) || (delta_z != 0)) {
+            freeze(false);
+            //Text.PrintStringNow(`Moving with delta_z=${delta_z}, pos.z=${pos.z}`,10);
+            PLAYER_CHAR.setCoordinates(pos.x+delta_x, pos.y+delta_y, pos.z+delta_z-1);
+            //PLAYER_CHAR.setCoordinates(pos.x, pos.y, pos.z);
+            freeze(true);
+        }
     }
     wait(10);
     if (CYCLES_SINCE_LAST_TOGGLE < 100000) CYCLES_SINCE_LAST_TOGGLE++;
+    if (CYCLES_SINCE_LAST_SPEED_CHANGE < 100000) CYCLES_SINCE_LAST_SPEED_CHANGE++;
 }
 
